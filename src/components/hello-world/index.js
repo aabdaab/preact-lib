eccaa1ebc9c0da8f0ca1e50d02cdb190dba0dd0f
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2020-12-03 17:37:51 +0100

    remove styles

--- src/components/hello-world/index.js
+++ src/components/hello-world/index.js
@@ -1,11 +1,10 @@
 import { h, Component } from "preact";
-import "./style.scss";
 
 export default class App extends Component {
   render(props) {
     return (
       <div>
-        <h1 style={{ color: props.color }}>Hello, World!</h1>
+        <h1>Hello, World!</h1>
       </div>
     );
   }
