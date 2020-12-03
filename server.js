b513c9ec168f452b9bc5de849a38687474d96376
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2020-12-03 17:27:23 +0100

    Create preact lib

---
+++ server.js
@@ -0,0 +1,19 @@
+const Koa = require('koa')
+const serve = require('koa-static')
+
+const app = new Koa();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(async (ctx, next) => {
+  const origin = `${ctx.request.protocol}://${ctx.request.host}`
+  ctx.set('AMP-Access-Control-Allow-Source-Origin', origin);
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Credentials', 'true');
+  await next()
+})
+
+app.use(serve(__dirname + '/build'));
+
+app.listen(PORT)
+console.log(`listening on port ${PORT}`)
